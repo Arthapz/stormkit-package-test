c90d9d44356abe3ee063dbdbd5faf885e6cd1c18
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-10-31 21:52:30 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+import std;
+
+import stormkit.Core;
+
+using namespace stormkit;
+
+auto main() -> int {
+  std::println("{}", as<float>(57));
+  return 0;
+}
---
+++ xmake.lua
@@ -0,0 +1,24 @@
+add_repositories("tapzcrew-repo https://github.com/Tapzcrew/xmake-repo main")
+add_requires(
+	"stormkit develop",
+	{
+		configs = {
+			image = false,
+			wsi = false,
+			log = false,
+			entities = false,
+			gpu = false,
+			components = { "core", "main" },
+		},
+	}
+)
+add_requires("glm", "frozen", "unordered_dense", "magic_enum", "tl_function_ref")
+add_requireconfs("**", { configs = { modules = true } })
+
+target("foo", function()
+  set_kind("binary")
+  set_languages("c++latest")
+	add_files("src/**.cpp")
+	add_packages("stormkit", "glm", "frozen", "unordered_dense", "magic_enum", "tl_function_ref")
+	set_policy("build.c++.modules", true)
+end)
